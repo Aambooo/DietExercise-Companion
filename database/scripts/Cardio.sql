965153f50ef87f527241b4b759d363fde95169ea
Aambooo <nabdabop10@gmail.com>
2025-08-13 16:37:47 +0545

    Database coreection

--- database/scripts/Cardio.sql
+++ database/scripts/Cardio.sql
@@ -1,4 +1,13 @@
-CREATE TABLE if not exists Cardio (
+--
+-- File generated with SQLiteStudio v3.4.17 on Wed Aug 13 16:24:33 2025
+--
+-- Text encoding used: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: Cardio
+CREATE TABLE IF NOT EXISTS Cardio (
 	Stage int not null,
     Body int not null,
     Sex int not null,
@@ -6,31 +15,18 @@ CREATE TABLE if not exists Cardio (
     Time varchar(255),
     primary key (Stage, Body, Sex)
 );
+INSERT INTO Cardio (Stage, Body, Sex, Sessions, Time) VALUES (0, 2, 0, 3, '12, 15, 12 minutes');
+INSERT INTO Cardio (Stage, Body, Sex, Sessions, Time) VALUES (1, 2, 0, 4, '22, 25, 22, 25 minutes');
+INSERT INTO Cardio (Stage, Body, Sex, Sessions, Time) VALUES (0, 3, 0, 3, '15, 20, 15 minutes');
+INSERT INTO Cardio (Stage, Body, Sex, Sessions, Time) VALUES (1, 3, 0, 4, '27, 30, 27, 30 minutes');
+INSERT INTO Cardio (Stage, Body, Sex, Sessions, Time) VALUES (0, 4, 0, 3, '20, 20, 20 minutes');
+INSERT INTO Cardio (Stage, Body, Sex, Sessions, Time) VALUES (1, 4, 0, 4, '35, 40, 30, 45 minutes');
+INSERT INTO Cardio (Stage, Body, Sex, Sessions, Time) VALUES (0, 2, 1, 3, '12, 15, 12 minutes');
+INSERT INTO Cardio (Stage, Body, Sex, Sessions, Time) VALUES (1, 2, 1, 4, '20, 22, 20, 22 minutes');
+INSERT INTO Cardio (Stage, Body, Sex, Sessions, Time) VALUES (0, 3, 1, 3, '15, 20, 15 minutes');
+INSERT INTO Cardio (Stage, Body, Sex, Sessions, Time) VALUES (1, 3, 1, 4, '25, 27, 25, 27 minutes');
+INSERT INTO Cardio (Stage, Body, Sex, Sessions, Time) VALUES (0, 4, 1, 3, '20, 20, 20 minutes');
+INSERT INTO Cardio (Stage, Body, Sex, Sessions, Time) VALUES (1, 4, 1, 4, '30, 35, 30, 35 minutes');
 
- /*
- Stage: 0, 1
- Body: thiếu cân: 0
-	   bình thường: 1
-       thừa cân: 2
-       tiền béo phì: 3
-       béo phì: 4
-Sex: nam: 0,
-     nữ: 1
- */
- 
- /* Query:
- SELECT * FROM eatandfit.Cardio WHERE Stage = ? and Body = ? and Sex = ?;
- */
- 
-INSERT INTO Cardio VALUES (0, 2, 0, 3, '12, 15, 12 phút'),
-					      (1, 2, 0, 4, '22, 25, 22, 25 phút'),
-					      (0, 3, 0, 3, '15, 20, 15 phút'),
-					      (1, 3, 0, 4, '27, 30, 27, 30 phút'),
-					      (0, 4, 0, 3, '20, 20, 20 phút'),
-					      (1, 4, 0, 4, '35, 40, 30, 45 phút'),
-					      (0, 2, 1, 3, '12, 15, 12 phút'),
-					      (1, 2, 1, 4, '20, 22, 20, 22 phút'),
-					      (0, 3, 1, 3, '15, 20, 15 phút'),
-					      (1, 3, 1, 4, '25, 27, 25, 27 phút'),
-					      (0, 4, 1, 3, '20, 20, 20 phút'),
-					      (1, 4, 1, 4, '30, 35, 30, 35 phút');
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
